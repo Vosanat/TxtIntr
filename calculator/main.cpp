c4405b61913af900a538e36f6108435bcda542b5
Vosanat <stud@virtdeb.local>
2023-10-18 22:08:43 +0300

    the great

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,80 +1,86 @@
 #include <iostream>
-#include <string>
-#include <unistd.h>
+#include <cstdlib>
+#include <getopt.h>
 #include <cmath>
-class Opts
-{
-private:
-    // константы с пределами значений
-    static constexpr int minx = -100;
-    static constexpr int maxx = 100;
-    static constexpr int miny = -100;
-    static constexpr int maxy = 100;
+#include <iomanip>
+using namespace std;
 
-    // атрибуты
-    int x; // длина пароля
-    int y; // количество паролей
+void print_help();
+bool compareStrings(const char *str1); //compareStrings(optarg)
 
-    void usage(unsigned err);
-public:
-    Opts(int argc, char **argv);
-    int getx()
-    {
-        return x;   
-    }
-    int gety()
-    {
-        return y;    
-    }
-};
-
-int main(int argc, char **argv)
-{
-    Opts decoder(argc, argv); 
-    return 0;
-}
-
-// реализация конструктора - весь разбор ПКС здесь
-Opts::Opts(int argc, char **argv)
+int main(int argc, char* argv[])
 {
     int opt;
-    bool flag = true;
-    while ((opt = getopt(argc, argv, "e:o: ")) != -1) {
+    double base = 0.0, exponent = 0.0;
+    bool calculate_exponent = false, calculate_power = false, flag = true;
+
+    while ((opt = getopt(argc, argv, "e:p:")) != -1) {
         switch (opt) {
-        case 'e':	// возводит число е в степень x
-            x = std::stol(optarg);
-            if (x < minx || x > maxx) {	// проверка на минимум и максимум
-                std::cerr << "x must be from " << minx << " to " << maxx << "\n";
-                usage(1);
+        case 'e':
+            flag = false;
+            if(compareStrings(optarg)) {
+                calculate_exponent = true;
+                exponent = atof(optarg);
+            } else {
+                cerr << "Error: Known option, but unknown argument.\n";
             }
-            std::cout << "exp(" << x << ") = " << exp(x) <<  std::endl;
             break;
-        case 'o': 	// возводит число х в степень у
-            y = std::stol(optarg);
-            if (y < miny || y > maxy) {	// проверка на минимум и максимум
-                std::cerr << "y must be from " << miny << " to " << maxy << "\n";
-                usage(2);
+        case 'p':
+            flag = false;
+            if(compareStrings(optarg)) {
+                calculate_power = true;
+                base = atof(optarg);
+                if (optind < argc) {
+                    exponent = atof(argv[optind]);
+                } else {
+                    cerr << "Error: Missing exponent value for -p option.\n";
+                    return 1;
+                }
+            } else {
+                cerr << "Error: Known option, but unknown argument.\n";
             }
-            std::cout << x <<"^" << y << " = " << pow(x,y) << std::endl;
             break;
-
-        default:	// неизвесный параметр
-            std::cerr << "Unknown option\n";
-            usage(4);
-
+        case '?':
+            cerr << "Error: Unknown option or missing argument.\n";
+            print_help();
+            return 1;
+        default:
+            cerr << "Unknown option\n";
+            break;
         }
-        flag = false;
     }
-    if (flag == true) {
-        usage(0);
+    if (calculate_exponent) {
+        double result = exp(exponent);
+        cout << "e^" << exponent << " = " << setprecision(6) << result << endl;
     }
+    if (calculate_power) {
+        double result = pow(base, exponent);
+        cout << base << "^" << exponent << " = " << setprecision(6) << result << endl;
+    }
+    if (flag == true) print_help();
+    return 0;
 }
-
-void Opts::usage(unsigned err)
+void print_help()
 {
-    std::cout << "Calculator_variant12_Tanasov\n";
-    std::cout << "Usage:  [-e x] [-o y] \n";
-    std::cout << "\t-e x - znachenie from -100 to 100; output: exp(x)\n";
-    std::cout << "\t-o y - znachenie from -100 to 100; output x^y\n";
-    exit(err);
+    cout << "Variant12_Tanasov\n";
+    cout << "Usage: calculator [OPTIONS]\n";
+    cout << "Options:\n";
+    cout << "  -e, --exponent VALUE    Calculate e^VALUE\n";
+    cout << "  -p, --power BASE POWER    Calculate BASE^POWER\n";
+}
+bool compareStrings(const char *str1) //compareStrings(optarg, size)
+{
+    bool flag = false;
+    const char digits [] = "0123456789.";
+    int size_digits = 10;
+    if (*str1 && digits) {
+        if (flag == true) return flag;
+        for(int j = 1; j <= size_digits; j++) {
+            if(*str1==digits[j]) {
+                flag = true;
+                break;
+            }
+        }
+    }
+    return flag;
 }
